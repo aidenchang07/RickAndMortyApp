a3e9589f570efb2646461ecbe93b5d4510003f8c
Aiden Chang <aidenchang@pcpartner.com>
2024-03-25 19:41:04 +0800

    Add ktor api
    https://rickandmortyapi.com/documentation/#get-a-single-location

--- app/src/main/java/com/adncoding/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/adncoding/rickandmortyapp/MainActivity.kt
@@ -6,19 +6,39 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import com.adncoding.network.Character
+import com.adncoding.network.KtorClient
 import com.adncoding.rickandmortyapp.ui.theme.RickAndMortyAppTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val ktorClient = KtorClient()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var character by remember { mutableStateOf<Character?>(null) }
+
+            LaunchedEffect(
+                key1 = Unit,
+                block = {
+                    character = ktorClient.getCharacter(1)
+                }
+            )
+
             RickAndMortyAppTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    
+                    Text(text = character?.name ?: "No character")
                 }
             }
         }
