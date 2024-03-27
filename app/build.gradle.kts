e2528ee381ce8c86d53712c879cae3a4ea4d5650
Aiden Chang <asdf62210@gmail.com>
2024-03-27 11:49:06 +0800

    Feat: Add Dagger - Hilt dependencies
    Solution:
    1. As title
    *Site: https://developer.android.com/training/dependency-injection/hilt-android?hl=zh-tw#application-class
    
    Finder: aiden
    Author: aiden

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -68,4 +70,8 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(project(":network"))
+
+    // Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
 }
