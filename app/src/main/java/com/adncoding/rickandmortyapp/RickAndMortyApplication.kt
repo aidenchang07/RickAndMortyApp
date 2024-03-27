e67c199ba6c4db030b798e9f456f252c4aa9bad2
Aiden Chang <asdf62210@gmail.com>
2024-03-27 12:40:19 +0800

    Feat: Add application use HiltAndroidApp
    Solution:
    1. As title
    
    Finder: aiden
    Author: aiden

---
+++ app/src/main/java/com/adncoding/rickandmortyapp/RickAndMortyApplication.kt
@@ -0,0 +1,10 @@
+package com.adncoding.rickandmortyapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by AidenChang 2024/03/27
+ */
+@HiltAndroidApp
+class RickAndMortyApplication: Application()
